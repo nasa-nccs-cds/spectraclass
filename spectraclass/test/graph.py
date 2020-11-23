a9c9a5c3572e3affc18eb7bff9b56f0f669eda6a
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-23 17:45:25 -0500

    IMplement spread.

--- spectraclass/test/graph.py
+++ spectraclass/test/graph.py
@@ -9,13 +9,14 @@ app = Spectraclass.instance()
 app.configure("spectraclass")
 n_neighbors = 5
 t0 = time.time()
+source = np.array( [30], dtype = np.int32 )
 
 project_dataset: xa.Dataset = DataManager.instance().loadCurrentProject("spectraclass")
 # table_cols = project_dataset.attrs['colnames']
 
 graph_data: xa.DataArray = project_dataset["reduction"]
 activation_flow = ActivationFlow.instance( graph_data, n_neighbors )
-activation_flow.spread( np.ndarray( [30] ) )
+activation_flow.spread( source )
 print( "Completed" )
 
 
