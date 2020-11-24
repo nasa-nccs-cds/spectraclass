bd271e8989787b3cfdab8c6662ab5c949ab2cfe3
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-24 13:44:00 -0500

    IMplement spread.

--- spectraclass/test/cugraph_test.py
+++ spectraclass/test/cugraph_test.py
@@ -2,15 +2,16 @@ import cudf
 from cuml.neighbors import NearestNeighbors
 import xarray as xa
 import numpy as np
+from spectraclass.gui.application import Spectraclass
 from spectraclass.data.manager import DataManager
+
+app = Spectraclass.instance()
+app.configure("spectraclass")
 
 project_dataset: xa.Dataset = DataManager.instance().loadCurrentProject("spectraclass")
 X: np.ndarray = project_dataset["reduction"].values[0:25,:]
 print(f"Input shape = {X.shape}")
-
-# build a cudf Dataframe
 X_cudf = cudf.DataFrame(X)
-
 print(f"X_cudf head = {X_cudf.head(10)}")
 
 # fit model
