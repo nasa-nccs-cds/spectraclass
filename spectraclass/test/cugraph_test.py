5edb27a4423a4131126f0424a1631b77e5b88edd
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-24 13:37:39 -0500

    IMplement spread.

--- spectraclass/test/cugraph_test.py
+++ spectraclass/test/cugraph_test.py
@@ -4,10 +4,12 @@ from cuml.datasets import make_blobs
 
 X, _ = make_blobs(n_samples=25, centers=5,
                   n_features=10, random_state=42)
-print(X)
+print(f"Input shape = {X.shape}")
 
 # build a cudf Dataframe
 X_cudf = cudf.DataFrame(X)
+
+print(f"X_cudf head = {X_cudf.head(10)}")
 
 # fit model
 model = NearestNeighbors(n_neighbors=3)
