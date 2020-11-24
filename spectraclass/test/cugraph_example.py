0dfa935d257c05751d83147dc5f299db10dc9fe4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-24 15:18:03 -0500

    Test NNeighbors graph

--- spectraclass/test/cugraph_example.py
+++ spectraclass/test/cugraph_example.py
@@ -4,6 +4,8 @@ from cuml.datasets import make_blobs
 
 nverts = 10000
 ndims = 16
+nneighbors = 5
+
 X, _ = make_blobs( n_samples=nverts, centers=5, n_features=ndims, random_state=42 )
 
 # build a cudf Dataframe
@@ -12,7 +14,7 @@ X_cudf = cudf.DataFrame(X)
 print( f"\nINPUT DATA:\n{X_cudf.head(20)}")
 
 # fit model
-model = NearestNeighbors(n_neighbors=3)
+model = NearestNeighbors(n_neighbors=nneighbors)
 model.fit(X)
 
 # get 3 nearest neighbors
