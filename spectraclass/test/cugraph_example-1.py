a621f44fc615f6f16b761fb27a077d07eb6d90df
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-24 15:55:09 -0500

    Test NNeighbors graph

--- spectraclass/test/cugraph_example-1.py
+++ spectraclass/test/cugraph_example-1.py
@@ -1,16 +1,20 @@
 import cudf, os
 from cuml.neighbors import NearestNeighbors
 from cuml.datasets import make_blobs
+import xarray as xa
+import numpy as np
+from spectraclass.gui.application import Spectraclass
+from spectraclass.data.manager import DataManager
 
-nverts = 500000
-ndims = 16
+app = Spectraclass.instance()
+app.configure("spectraclass")
 nneighbors = 5
 
-X, _ = make_blobs( n_samples=nverts, centers=5, n_features=ndims, random_state=42 )
+project_dataset: xa.Dataset = DataManager.instance().loadCurrentProject("spectraclass")
+X = project_dataset["reduction"].values
+print(f"Dataset input shape = {X.shape}, ")
 
-# build a cudf Dataframe
 X_cudf = cudf.DataFrame(X)
-
 print( f"\nINPUT DATA:\n{X_cudf.head(10)}")
 
 # fit model
