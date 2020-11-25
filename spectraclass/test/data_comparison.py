f65a85eec107ebf3db1ec6c274401af85243b0ae
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-25 14:46:52 -0500

    Add NNeighbors test

--- spectraclass/test/data_comparison.py
+++ spectraclass/test/data_comparison.py
@@ -14,15 +14,18 @@ project_dataset: xa.Dataset = DataManager.instance().loadCurrentProject("spectra
 spectraclass_data = project_dataset["reduction"].values
 print(f"Spectraclass input shape = {spectraclass_data.shape}")
 sc_cudf_data = cudf.DataFrame(spectraclass_data)
-
+scindx = sc_cudf_data.index
 print( f"Spectraclass dataframe: ndim = {sc_cudf_data.ndim}, size = {sc_cudf_data.size}, col shape = {sc_cudf_data.columns.shape}, shape = {sc_cudf_data.index.shape}, np-shape = {sc_cudf_data.values.shape}")
-print( f"  --> device_data.index.__class__ = {sc_cudf_data.index.__class__}")
+print( f"  --> index: shape = {scindx.shape}, ndim = {scindx.ndim}, dtype = {scindx.dtype}, size = {scindx.size}, is_contiguous = {scindx.is_contiguous}")
 
 n_samples = spectraclass_data.shape[0]
 n_features = spectraclass_data.shape[1]
 scipy_data, _ = make_blobs(n_samples=n_samples,  n_features=n_features,  centers=5, random_state=random_state)
 print(f"Scipy input shape = {scipy_data.shape}")
 scipy_cudf_data = cudf.DataFrame(scipy_data)
+spyindx = scipy_cudf_data.index
 
 print( f"scipy dataframe: ndim = {scipy_cudf_data.ndim}, size = {scipy_cudf_data.size}, col shape = {scipy_cudf_data.columns.shape}, shape = {scipy_cudf_data.index.shape}, np-shape = {scipy_cudf_data.values.shape}")
-print( f"  --> device_data.index.__class__ = {scipy_cudf_data.index.__class__}")
+print( f"  --> index: shape = {spyindx.shape}, ndim = {spyindx.ndim}, dtype = {spyindx.dtype}, size = {spyindx.size}, is_contiguous = {spyindx.is_contiguous}")
+
+print( f"\nDifference = {scindx.difference(spyindx)}")
