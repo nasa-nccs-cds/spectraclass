2745f549a3b05ecde07bfe87acf18a47cfa74f53
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-27 12:36:33 -0500

    Add NNeighbors test

---
+++ spectraclass/test/create_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import xarray as xa
+from spectraclass.gui.application import Spectraclass
+from spectraclass.data.manager import DataManager
+
+app = Spectraclass.instance()
+app.configure("spectraclass")
+nrows = 20000
+
+project_dataset: xa.Dataset = DataManager.instance().loadCurrentProject("spectraclass")
+input_data: xa.DataArray = project_dataset["reduction"]
+input_data[0:nrows,:].to_dataframe("spectral_embedding").to_csv("spectraclass_data.csv")
+
