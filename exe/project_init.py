6b89634e38eff59f59d4e17a4e54b5c3d82426f0
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-02-03 17:25:24 -0500

    Update data init

--- exe/project_init.py
+++ exe/project_init.py
@@ -2,11 +2,11 @@ from spectraclass.data.base import DataManager
 import sys
 
 if len(sys.argv) != 3:
-    print( f"Usage: {sys.argv[0]} <project> <mode>")
+    print( f"Usage: {sys.argv[0]} <mode> <project>")
 else:
 
-    project: str = sys.argv[1]    #   e.g. 'demo1', 'demo2', 'demo3', or 'demo4'
-    mode: str = sys.argv[2]       #   e.g. 'swift', 'tess', 'desis', or 'aviris'
+    mode: str = sys.argv[1]       #   e.g. 'swift', 'tess', 'desis', or 'aviris'
+    project: str = sys.argv[2]    #   e.g. 'demo1', 'demo2', 'demo3', or 'demo4'
 
     dm: DataManager = DataManager.initialize( project, mode )
     dm.prepare_inputs()
