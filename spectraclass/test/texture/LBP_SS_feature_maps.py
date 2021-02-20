3d044ddb56f1c8a12a35f6562d09f12a9267d98b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-02-20 12:48:36 -0500

    Add NN Graph to LBP test

--- spectraclass/test/texture/LBP_SS_feature_maps.py
+++ spectraclass/test/texture/LBP_SS_feature_maps.py
@@ -1,17 +1,22 @@
 import os, time, random, numpy as np
 from typing import List, Union, Tuple, Optional, Dict, Callable, Iterable
 import matplotlib.pyplot as plt
-from sklearn.decomposition import KernelPCA
 import scipy.stats as stats
+from pynndescent import NNDescent
 from matplotlib.figure import Figure
 from matplotlib.image import AxesImage
 from matplotlib.axes import Axes, BarContainer
-from sklearn.metrics.pairwise import chi2_kernel
 import xarray as xa
 from skimage.feature import texture as sktex
 from skimage.morphology import disk, ball
 from skimage.filters.rank import windowed_histogram
 from spectraclass.test.texture.util import load_test_data
+
+def getProbabilityGraph( data: np.ndarray, nneighbors: int ) -> NNDescent:    # data: array, shape = (n_samples, n_features)
+    n_trees = 5 + int(round((data.shape[0]) ** 0.5 / 20.0))
+    n_iters = max(5, 2 * int(round(np.log2(data.shape[0]))))
+    kwargs = dict(n_trees=n_trees, n_iters=n_iters, n_neighbors=nneighbors, max_candidates=60, verbose=True, metric="wasserstein")
+    return  NNDescent(data, **kwargs)
 
 fig, axs = plt.subplots( 2, 4 )
 def plot( iP: int, data: np.ndarray, title: str ) -> AxesImage:
@@ -32,21 +37,20 @@ R = 1.5
 P = round( 8 * R )
 method = "uniform"  # "var" "ror" "uniform"
 hist_disk_radius = 2
-
-lbp: np.ndarray = sktex.local_binary_pattern( texband.data, P, R, method ).astype(np.int)
-print( f" calculated LBP, shape = {lbp.shape}, range = [ {lbp.min()}, {lbp.max()} ] ")
-
-hist_array: np.ndarray = windowed_histogram( lbp, disk( hist_disk_radius )  )
-[ys,xs,nbin] = hist_array.shape
-print( f" calculated hist_array, shape = {hist_array.shape}")
+n_graph_neighbors = 5
 
 t0 = time.time()
-K: np.ndarray = chi2_kernel( hist_array.reshape([ys*xs,nbin]), gamma=.5 )
-dt = time.time() - t0
-print( f"Computed Kernel in {dt} sec ({dt/60} min), K shape = {K.shape}, K range = [ {K.min()}, {K.max()} ]")
+lbp: np.ndarray = sktex.local_binary_pattern( texband.data, P, R, method ).astype(np.int)
+print( f" calculated LBP in time {time.time()-t0} sec, shape = {lbp.shape}, range = [ {lbp.min()}, {lbp.max()} ] ")
 
-transformer = KernelPCA(n_components=8, kernel='precomputed')
-reduced = transformer.fit_transform(K)
+t1 = time.time()
+hist_array: np.ndarray = windowed_histogram( lbp, disk( hist_disk_radius )  )
+[ys,xs,nbin] = hist_array.shape
+print( f" calculated hist_array in time {time.time()-t1} sec, shape = {hist_array.shape}")
+
+t2 = time.time()
+graph = getProbabilityGraph( hist_array.reshape( [ys*xs, nbin] ), n_graph_neighbors )
+print( f" calculated ProbabilityGraph in time {time.time()-t2} sec")
 
 plot( 0, texband.data, f"{data_type}-{iBand}" )
 plot( 1, lbp, f"LBP: P={P}, R={R}" )
--- spectraclass/test/texture/util.py
+++ spectraclass/test/texture/util.py
@@ -1,4 +1,4 @@
-import os, random, numpy as np
+import os, time, random, numpy as np
 from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type
 import xarray as xa
 TEST_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -10,6 +10,8 @@ def scale(x, out_range=(-1, 1)):
 
 
 def load_test_data( dataset_type: str, dsid: str, data_type: str, ilayer: int ) -> xa.DataArray:
+    t0 = time.time()
     data_file = os.path.join(TEST_DIR, "data", dataset_type, f"{dsid}_{data_type}_{ilayer}.nc4")
     dataset = xa.load_dataset(data_file)
+    print(f" loaded test data in time {time.time() - t0} sec")
     return dataset['data']
