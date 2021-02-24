2aa7500781ccb8288544e7efc7a251ea625b19b2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-02-24 15:59:22 -0500

    Add GLCM textures

--- spectraclass/test/texture/GLCM.py
+++ spectraclass/test/texture/GLCM.py
@@ -4,6 +4,7 @@ from spectraclass.test.texture.util import *
 from skimage.feature import greycomatrix, greycoprops
 from skimage.util import apply_parallel, img_as_ubyte
 from skimage.filters import gaussian
+from skimage.transform import pyramid_expand
 import math
 pi4 = math.pi/4
 
@@ -24,11 +25,18 @@ def rescale( X: np.ndarray, bin_size = 1 ) -> np.ndarray:
     Xs = (X - X.min()) / (X.max() - X.min())
     return img_as_ubyte( Xs ) // bin_size
 
+def unpack( image: np.ndarray, grid_size: int, offset: int ):
+    fdata = image[0::grid_size,offset::grid_size].reshape( [ s//grid_size for s in image.shape ] )
+    return pyramid_expand(fdata, upscale=grid_size, sigma=None, order=1, mode='reflect', cval=0, multichannel=False, preserve_range=False)
+
 t0 = time.time()
-def glcm_feature( feature: str, patch: np.ndarray ):
+def glcm_feature( patch: np.ndarray ):
+    if patch.size == 1: return np.zeros_like( patch, dtype = np.float )
     glcm = greycomatrix( patch, glcm_distances, glcm_angles, levels, symmetric=True, normed=True )
-    f = greycoprops( glcm, feature )[0, 0]
-    return np.full( patch.shape, f )
+    h = greycoprops( glcm, 'homogeneity' )[0, 0]
+    rv: np.ndarray = np.full( patch.shape, h, dtype = np.float )
+    rv[overlap,overlap] = greycoprops(glcm, 'energy')[0, 0]
+    return rv
 
 raw_image: np.ndarray = load_test_data( dataset_type, dsid, data_type, iLayer ).data[:block_size,:block_size]
 image: np.ndarray = rescale( raw_image, bin_size )
@@ -36,11 +44,10 @@ print( f"Loaded {data_type} image, band = {iLayer}, shape = {image.shape}, range
 
 t1= time.time()
 
-h = apply_parallel( partial( glcm_feature, 'homogeneity' ), image, chunks=grid_size, depth=overlap, mode='reflect' )
-homogeneity = gaussian( h, sigma = grid_size/2,  mode='reflect' )
+features = apply_parallel( glcm_feature, image, chunks=grid_size, depth=overlap, mode='reflect' )
 
-e = apply_parallel( partial( glcm_feature, 'energy' ), image, chunks=grid_size, depth=overlap, mode='reflect' )
-energy = gaussian( e, sigma = grid_size/2,  mode='reflect' )
+energy = unpack( features, grid_size, 0 )
+homogeneity = unpack( features, grid_size, 1 )
 
 print( f"Computed glcm_features in time {time.time()-t1} sec")
 
