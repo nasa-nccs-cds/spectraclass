0f31044687d14b2213bf9a375a5c7c127c9c2042
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-09-12 17:31:18 -0600

    ADD tile selection

--- spectraclass/gui/spatial/widgets/tiles.py
+++ spectraclass/gui/spatial/widgets/tiles.py
@@ -1,6 +1,11 @@
 import numpy as np
-import sys
+import sys, numbers
 from copy import deepcopy
+from spectraclass.gui.spatial.widgets.crs import get_ccrs
+import cartopy.crs as ccrs
+import pyproj.crs as pcrs
+from scipy import stats as sps
+import xarray as xa
 from typing import List, Union, Tuple, Optional, Dict, Callable
 from matplotlib.backend_bases import MouseEvent
 from matplotlib.axes import Axes
@@ -56,12 +61,12 @@ class TileSelector:
             self._ax.draw_artist(self.rect)
             if self._selection_rect is not None:
                 self._ax.draw_artist( self._selection_rect )
-            self.canvas.blit(self._ax.bbox)
+            self.canvas.draw()
         else:
             if self._background is not None:
                 self.canvas.restore_region(self._background)
                 self._background = None
-                self.canvas.blit(self._ax.bbox)
+                self.canvas.draw()
 
     def deactivate(self):
         self._active = False
@@ -82,6 +87,91 @@ class TileSelector:
         self.rect.set_visible( False )
         self.canvas.draw()
         self.canvas = None
+
+class TileManager:
+
+    def __init__(self, file_path: str, tile_size: int, **kwargs  ):
+        data = xa.open_rasterio( file_path )
+        assert data.ndim in [2, 3], f"Can't defin Tile with {data.ndim} dims"
+        self._tile_size = tile_size
+        nodata_fill = kwargs.get( 'nodata_fill', None )
+        if nodata_fill is not None:
+            data = self.fill_nodata( data, nodata_fill )
+        self._data: xa.DataArray = data.expand_dims( {"band":1}, 0 ) if data.ndim == 2 else data
+        self._xc: np.ndarray = data.coords[ data.dims[2] ].data
+        self._yc: np.ndarray = data.coords[ data.dims[1] ].data
+
+    def vrange(self) -> Tuple[float,float]:
+        return ( self._data.data.min(), self._data.data.max() )
+
+    def crs(self) -> ccrs.CRS:
+        proj4_attrs: Dict = self.to_proj4( self._data.attrs["crs"] )
+        return get_ccrs(proj4_attrs)
+
+    @staticmethod
+    def to_proj4( crs: str ) -> Dict:
+        tups = [ kv.split('=') for kv in crs.split('+') ]
+        return { t[0].strip(): t[1].strip() for t in tups if (len(t) == 2) }
+
+    @staticmethod
+    def fill_nodata( da: xa.DataArray, fill_val ) -> xa.DataArray:
+        nodata = da.attrs.get('nodatavals')
+        if nodata is not None:
+            filled_data: np.ndarray = np.where( da.data == nodata, fill_val, da.data )
+            return da.copy( data=filled_data )
+        else: return da
+
+    def downscale( self, block_size: int, origin: str ) -> xa.DataArray:
+        nbands, ishp = self._data.shape[0], self._data.shape[1:]
+        new_shape = ishp[0]//block_size, ishp[1]//block_size
+        nsh = [ ishp[0]//new_shape[0], ishp[1]//new_shape[1] ]
+        sh = nbands, new_shape[0], nsh[0], new_shape[1], nsh[1]
+        sxa: xa.DataArray = self._data[ :, :new_shape[0]*nsh[0], :new_shape[1]*nsh[1] ]
+        ta: np.ndarray = sxa.data.reshape(sh)
+        tas: np.ndarray = ta.transpose( (0,1,3,2,4) ).reshape( (nbands, new_shape[0], new_shape[1], nsh[0]*nsh[1]) )
+        if issubclass( self._data.dtype.type, numbers.Integral ):
+            modes, counts = sps.mode( tas, axis=3, nan_policy='omit' )
+        else: modes = tas.mean( axis=3 )
+        dxa: xa.DataArray = sxa[ :, ::block_size, ::block_size ]
+        dxa = dxa.copy( data = modes.reshape( dxa.shape ) )
+        transform: List = list(self._data.transform)
+        for i in [0,1,3,4]: transform[i] = transform[i]*block_size
+        dxa.attrs['transform'] = transform
+        dxa.attrs['extent'] = self.extent( transform, new_shape, origin )
+        return dxa
+
+    @staticmethod
+    def idx_of_nearest(array: np.ndarray, value: float):
+        array = np.asarray(array)
+        return (np.abs(array - value)).argmin()
+
+    @staticmethod
+    def dsize( transform: Union[List, Tuple], isize: int ) -> Tuple[float,float]:
+        return (  abs( isize * transform[0] + isize * transform[1] ),  abs( isize * transform[3] + isize * transform[4] ) )
+
+    @staticmethod
+    def extent( transform: Union[List, Tuple], shape: Union[List, Tuple], origin: str ):
+        (sy,sx) = (shape[1],shape[2]) if len(shape) == 3 else (shape[0],shape[1])
+        ext =  [transform[2], transform[2] + sx * transform[0] + sy * transform[1],
+                transform[5], transform[5] + sx * transform[3] + sy * transform[4]]
+        if origin == "upper": ( ext[2], ext[3] ) = ( ext[3], ext[2] )
+        return ext
+
+    def get_tile(self, dloc: List[float] = None, origin: str = "upper" ) -> xa.DataArray:
+        if dloc is None:
+            loc = [ self._data.shape[2]//2, self._data.shape[1]//2 ]
+            dloc = [ self._xc[loc[0]], self._yc[loc[1]],  ]
+        else:
+            loc = [ self.idx_of_nearest(self._xc,dloc[0]), self.idx_of_nearest(self._yc,dloc[1]) ]
+        if origin == "upper":  tile = self._data[:, loc[1]-self._tile_size:loc[1], loc[0]:loc[0]+self._tile_size ]
+        else:                  tile = self._data[:, loc[1]:loc[1]+self._tile_size, loc[0]:loc[0]+self._tile_size ]
+        transform: List = list(self._data.attrs['transform'])
+        transform[2], transform[5] = dloc[0], dloc[1]
+        tile.attrs['transform'] = transform
+        tile.attrs['extent'] = self.extent( transform, tile.shape, origin )
+        return tile
+
+
 
 if __name__ == '__main__':
 
--- spectraclass/test/map_overlay_test.py
+++ spectraclass/test/map_overlay_test.py
@@ -10,94 +10,16 @@ import xarray as xa
 from affine import Affine
 from matplotlib.backend_bases import MouseEvent
 from matplotlib.image import AxesImage
-from affine import Affine
 import numpy as np
 from matplotlib.axes import Axes
-from scipy import stats as sps
 import matplotlib.pyplot as plt
-from spectraclass.gui.spatial.widgets.tiles import TileSelector
+from spectraclass.gui.spatial.widgets.tiles import TileSelector, TileManager
 import cartopy.crs as ccrs
-from spectraclass.gui.spatial.widgets.crs import get_ccrs
-from typing import List, Union, Tuple, Optional, Dict, Iterable
 
-import pyproj.crs as pcrs
-import rasterio as rio
+from typing import List, Union, Tuple, Optional, Dict, Iterable
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.ticker as mticker
-
-def to_proj4( crs: str ) -> Dict:
-    tups = [ kv.split('=') for kv in crs.split('+') ]
-    return { t[0].strip(): t[1].strip() for t in tups if (len(t) == 2) }
-
-def idx_of_nearest( array: np.ndarray, value: float ):
-    array = np.asarray(array)
-    return (np.abs(array - value)).argmin()
-
-class Tile:
-
-    def __init__(self, data: xa.DataArray, tile_size: int ):
-        assert data.ndim in [2,3], f"Can't defin Tile with {data.ndim} dims"
-        self._data: xa.DataArray = data.expand_dims( {"band":1}, 0 ) if data.ndim == 2 else data
-        self._xc: np.ndarray = data.coords[ data.dims[2] ].data
-        self._yc: np.ndarray = data.coords[ data.dims[1] ].data
-        self._tile_size: int = tile_size
-
-    @staticmethod
-    def dsize( transform: Union[List, Tuple], isize: int ) -> Tuple[float,float]:
-        return (  abs( isize * transform[0] + isize * transform[1] ),  abs( isize * transform[3] + isize * transform[4] ) )
-
-    @staticmethod
-    def extent( transform: Union[List, Tuple], shape: Union[List, Tuple], origin: str ):
-        (sy,sx) = (shape[1],shape[2]) if len(shape) == 3 else (shape[0],shape[1])
-        ext =  [transform[2], transform[2] + sx * transform[0] + sy * transform[1],
-                transform[5], transform[5] + sx * transform[3] + sy * transform[4]]
-        if origin == "upper": ( ext[2], ext[3] ) = ( ext[3], ext[2] )
-        return ext
-
-    def get_full_extent(self):
-        dx, dy = (self._xc[-1]-self._xc[-2]), (self._yc[-1]-self._yc[-2])
-        return self._xc[0], self._xc[-1]+dx, self._yc[0], self._yc[-1]+dy
-
-    def get_tile(self, dloc: List[float] = None, origin: str = "upper" ) -> xa.DataArray:
-        if dloc is None:
-            loc = [ self._data.shape[2]//2, self._data.shape[1]//2 ]
-            dloc = [ self._xc[loc[0]], self._yc[loc[1]],  ]
-        else:
-            loc = [ idx_of_nearest(self._xc,dloc[0]), idx_of_nearest(self._yc,dloc[1]) ]
-        iext = [ loc[i]+self._tile_size for i in (0,1)  ]
-        tile = self._data[ :, loc[1]:iext[1], loc[0]:iext[0] ]
-        transform: List = list(self._data.attrs['transform'])
-        transform[2], transform[5] = dloc[0], dloc[1]
-        tile.attrs['transform'] = transform
-        tile.attrs['extent'] = self.extent( transform, tile.shape, origin )
-        return tile
-
-def fill_nodata( da: xa.DataArray, fill_val ) -> xa.DataArray:
-    nodata = da.attrs.get('nodatavals')
-    if nodata is not None:
-        filled_data: np.ndarray = np.where( da.data == nodata, fill_val, da.data )
-        return da.copy( data=filled_data )
-    else: return da
-
-def downscale( geo_xarray: xa.DataArray, block_size: int, origin: str ) -> xa.DataArray:
-    nbands, ishp = geo_xarray.shape[0], geo_xarray.shape[1:]
-    new_shape = ishp[0]//block_size, ishp[1]//block_size
-    nsh = [ ishp[0]//new_shape[0], ishp[1]//new_shape[1] ]
-    sh = nbands, new_shape[0], nsh[0], new_shape[1], nsh[1]
-    sxa: xa.DataArray = geo_xarray[ :, :new_shape[0]*nsh[0], :new_shape[1]*nsh[1] ]
-    ta: np.ndarray = sxa.data.reshape(sh)
-    tas: np.ndarray = ta.transpose( (0,1,3,2,4) ).reshape( (nbands, new_shape[0], new_shape[1], nsh[0]*nsh[1]) )
-    if issubclass( geo_xarray.dtype.type, numbers.Integral ):
-        modes, counts = sps.mode( tas, axis=3, nan_policy='omit' )
-    else: modes = tas.mean( axis=3 )
-    dxa: xa.DataArray = sxa[ :, ::block_size, ::block_size ]
-    dxa = dxa.copy( data = modes.reshape( dxa.shape ) )
-    transform: List = list(geo_xarray.transform)
-    for i in [0,1,3,4]: transform[i] = transform[i]*block_size
-    dxa.attrs['transform'] = transform
-    dxa.attrs['extent'] = Tile.extent( transform, new_shape, origin )
-    return dxa
 
 iband = 0
 block_size = 100
@@ -106,36 +28,31 @@ blocks_per_tile = 5
 tile_size = block_size*blocks_per_tile
 LabelDataFile = "/Users/tpmaxwel/GDrive/Tom/Data/ChesapeakeLandUse/CalvertCounty/CALV_24009_LandUse.tif"
 
-da: xa.DataArray = fill_nodata( xa.open_rasterio( LabelDataFile ), -1 )
-downscaled_data: xa.DataArray = downscale( da, block_size, origin )
-tile = Tile( da, tile_size )
-
-proj4_attrs: Dict = to_proj4( da.attrs["crs"] )
-proj_crs = pcrs.CRS( proj4_attrs )
-cart_crs: ccrs.CRS = get_ccrs( proj4_attrs )
+tmgr = TileManager( LabelDataFile, tile_size, nodata_fill=-1 )
+crs: ccrs.CRS = tmgr.crs()
 
 fig = plt.figure( figsize=(16,8) )
-ax0: Axes = fig.add_subplot( 121, projection=cart_crs )
-tile_array0 = tile.get_tile( None, origin )
+ax0: Axes = fig.add_subplot( 121, projection=crs )
+tile_array0 = tmgr.get_tile( None, origin )
 tile_data = tile_array0.data[iband]
-vmin, vmax = da.data.min(), da.data.max(),
-img0: AxesImage = ax0.imshow( tile_data, transform=cart_crs, origin=origin, cmap="tab20", extent=tile_array0.attrs["extent"], vmin=vmin, vmax=vmax )
+vr = tmgr.vrange()
+img0: AxesImage = ax0.imshow( tile_data, transform=crs, origin=origin, cmap="tab20", extent=tile_array0.attrs["extent"], vmin=vr[0], vmax=vr[1] )
 
 def on_tile_selection( event: MouseEvent ):
     dloc = [ event.xdata, event.ydata ]
-    tile_array = tile.get_tile( dloc, origin )
+    tile_array = tmgr.get_tile( dloc, origin )
     tile_ext = tile_array.attrs["extent"]
     img0.set_extent( tile_ext )
     tile_data = tile_array.data[iband]
-    print(f"on_tile_selection: loc = {dloc}, dshape={tile_data.shape}, extent = {tile_ext}, vrange = [ {tile_data.min()}, {tile_data.max()} ]")
     img0.set_data( tile_data )
     img0.figure.canvas.draw()
     img0.figure.canvas.flush_events()
 
+downscaled_data: xa.DataArray = tmgr.downscale( block_size, origin )
 ax1 = fig.add_subplot( 122   )
-img1: AxesImage = ax1.imshow( downscaled_data.data[iband], origin='upper', cmap="tab20", vmin=vmin, vmax=vmax )
+img1: AxesImage = ax1.imshow( downscaled_data.data[iband], origin='upper', cmap="tab20", vmin=vr[0], vmax=vr[1] )
 img1.set_extent( downscaled_data.extent )
-rsize = Tile.dsize( downscaled_data.transform, blocks_per_tile )
+rsize = tmgr.dsize( downscaled_data.transform, blocks_per_tile )
 ts = TileSelector( ax1, rsize, on_tile_selection )
 ts.activate()
 plt.show()
