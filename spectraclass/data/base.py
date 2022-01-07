b68600e3f55c9c9139b95e363aba2261d97c9c5b
Thomas Maxwell <thomas.maxwell@nasa.gov>
2022-01-07 12:16:45 -0700

    Update config for jupyter

--- spectraclass/data/base.py
+++ spectraclass/data/base.py
@@ -76,9 +76,9 @@ class DataManager(SCSingletonConfigurable):
 
     @classmethod
     def initialize(cls, name: str, mode: str ):
+        lgm().init_logging(name, mode)
         dataManager = cls.instance()
         dataManager._configure_( name, mode )
-        lgm().init_logging(name, mode)
         if mode.lower() not in cls._mode_data_managers_: raise Exception( f"Mode {mode} is not defined, available modes = {cls._mode_data_managers_.keys()}")
         dataManager._mode_data_manager_ = cls._mode_data_managers_[ mode.lower() ].instance()
         lgm().log("Logging configured")
