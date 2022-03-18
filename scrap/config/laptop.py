f9a9e9d79745a3b1974594f2b393bfda0afcea58
Thomas Maxwell <thomas.maxwell@nasa.gov>
2022-03-18 11:09:29 -0600

    Update for laptop

---
+++ scrap/config/laptop.py
@@ -0,0 +1,26 @@
+c.ModeDataManager.data_dir = "~/Development/Data/"
+c.ModeDataManager.cache_dir = "~/Development/Cache"
+c.ModeDataManager.dset_name = ""
+c.ModeDataManager.model_dims = 24
+c.ModeDataManager.reduce_nepochs = 5
+c.ModeDataManager.reduce_sparsity = 0.0
+c.ModeDataManager.subsample_index = 5
+c.ModeDataManager.reduce_method = "Autoencoder"
+c.ModeDataManager.class_file = "NONE"
+c.DataManager.proc_type = "cpu"
+c.EmbeddingManager.alpha = 0.9
+c.EmbeddingManager.init = "random"
+c.EmbeddingManager.nepochs = 200
+c.EmbeddingManager.target_weight = 0.5
+c.ReductionManager.alpha = 0.9
+c.ReductionManager.init = "random"
+c.ReductionManager.loss = "mean_squared_error"
+c.ReductionManager.ndim = 3
+c.ReductionManager.nepochs = 200
+c.ReductionManager.target_weight = 0.5
+c.ActivationFlowManager.metric = "cosine"
+c.ActivationFlowManager.nneighbors = 5
+c.PointCloudManager.color_map = "gist_rainbow"
+c.MapManager.init_band = 0
+c.TextureManager.textures = [{'type': 'gabor', 'bands': [1, 3], 'nfeatures': 1}, {'type': 'glcm', 'bands': [1], 'features': ['homogeneity', 'energy']}]
+
