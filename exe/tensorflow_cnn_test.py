8ec488e129e6123e5b8c6d465e0c174abd8d1461
Thomas Maxwell <thomas.maxwell@nasa.gov>
2022-10-25 16:51:06 -0600

    Add tensorflow test

--- exe/tensorflow_cnn_test.py
+++ exe/tensorflow_cnn_test.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 from spectraclass.model.labels import lm
 from spectraclass.learn.models.spatial import SpatialModelWrapper
 from spectraclass.data.base import DataManager
+from spectraclass.data.spatial.tile.manager import TileManager
+from spectraclass.data.spatial.modes import AvirisDataManager
 
 dm: DataManager = DataManager.initialize( "img_mgr", 'aviris' )
 location = "desktop"
@@ -13,6 +15,40 @@ elif location == "desktop":
     dm.modal.cache_dir = "/Volumes/Shared/Cache"
     dm.modal.data_dir = "/Users/tpmaxwel/Development/Data/Aviris"
 else: raise Exception( f"Unknown location: {location}")
+
+block_size = 150
+method = "aec" # "vae"
+model_dims = 32
+version = "v2v2"
+month = "201908"
+
+dm.modal.data_dir = "/Users/tpmaxwel/Development/Data/Aviris/adapt"
+dm.modal.ext =  "_img"
+dm.use_model_data = True
+dm.proc_type = "skl"
+dm.modal.images_glob = f"ang{month}*rfl/ang*_rfl_{version}/ang*_corr_{version}_img"
+TileManager.block_size = block_size
+TileManager.block_index = [1,7]
+AvirisDataManager.version = version
+dm.modal.valid_aviris_bands = [ [5,193], [214,283], [319,10000] ]
+dm.modal.model_dims = model_dims
+dm.modal.reduce_method = method
+dm.modal.reduce_nepoch = 3
+dm.modal.reduce_focus_nepoch = 10
+dm.modal.reduce_niter = 1
+dm.modal.reduce_focus_ratio = 10.0
+dm.modal.reduce_dropout = 0.0
+dm.modal.reduce_learning_rate = 1e-4
+dm.modal.refresh_model = False
+dm.modal.modelkey = f"b{block_size}.{method}"
+
+dm.loadCurrentProject()
+classes = [ ('Class-1', "cyan"),
+            ('Class-2', "green"),
+            ('Class-3', "magenta"),
+            ('Class-4', "blue")]
+
+lm().setLabels( classes )
 
 input_shape = SpatialModelWrapper.get_input_shape()
 nclasses = lm().nLabels
