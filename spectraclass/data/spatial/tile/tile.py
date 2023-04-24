10728495426fb80dc3de82dffcb6575144fdb17f
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-04-24 14:17:03 -0600

    Add support for NEON AGB data.

--- spectraclass/data/spatial/tile/tile.py
+++ spectraclass/data/spatial/tile/tile.py
@@ -755,11 +755,12 @@ class Block(DataContainer):
             nodata = point_data.attrs.get('_FillValue',np.nan)
             point_data = point_data if np.isnan(nodata) else point_data.where(point_data != nodata, np.nan)
 
-        vcnts = [ nnan( point_data.values[ic] ) for ic in range( point_data.shape[0] ) ]
-        pmask: np.ndarray = (np.array(vcnts) < point_data.shape[1]*0.5)
-        lgm().log(f" pmask shp={shp(pmask)}, nvalid={np.count_nonzero(pmask)})  ")
+        bvcnts = [ nnan( point_data.values[:,ic] ) for ic in range( point_data.shape[1] ) ]
+        bmask: np.ndarray = (np.array(bvcnts) < point_data.shape[0]*0.5)
+        lgm().log(f" bmask shp={shp(bmask)}, nvalid={np.count_nonzero(bmask)})  ")
 
-        filtered_point_data: xa.DataArray = point_data[pmask]
+        filtered_point_data: xa.DataArray = point_data[:,bmask]
+        lgm().log(f" band-filtered point_data shape = {filtered_point_data.shape} ")
 
         vcnts0 = [ nnan( filtered_point_data.values[ic] )   for ic in range( filtered_point_data.shape[0] ) ]
         vcnts1 = [ nnan( filtered_point_data.values[:,ic] ) for ic in range( filtered_point_data.shape[1] ) ]
