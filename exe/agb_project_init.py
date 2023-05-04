9e8e522ffe05f218823310b992f6f850b828a607
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-05-04 11:45:06 -0600

    Build out holoviews gui.

--- exe/agb_project_init.py
+++ exe/agb_project_init.py
@@ -11,8 +11,8 @@ roi = "541567.6_4136443.0_542567.6_4137443.0"
 dm.proc_type = "cpu"
 dm.modal.images_glob = f"AGB/test/{version}/MLBS_{year}_{roi}/MLBS_{year}_Reflectance_reflectance_warp.tif"
 TileManager.block_size = block_size
-TileManager.reprocess = True
-dm.modal.refresh_model = True
+TileManager.reprocess = False
+dm.modal.refresh_model = False
 dm.modal.model_dims = model_dims
 dm.modal.modelkey = f"agp.neon.{version}.{year}.{roi}.{block_size}"
 
