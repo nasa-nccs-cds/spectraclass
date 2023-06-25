0f49f30a7ae9c3165a10ff5106f463d67039cd20
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-06-25 13:58:14 -0600

    Add vae

--- spectraclass/reduction/vae/base.py
+++ spectraclass/reduction/vae/base.py
@@ -1,29 +1,67 @@
-from .types_ import *
-from torch import nn
-from abc import abstractmethod
+import torch; torch.manual_seed(0)
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils
+import torch.distributions
+import numpy as np
+device = "cpu"
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(Decoder, self).__init__()
+        self.linear1 = nn.Linear(latent_dims, 512)
+        self.linear2 = nn.Linear(512, 784)
+
+    def forward(self, z):
+        z = F.relu(self.linear1(z))
+        z = torch.sigmoid(self.linear2(z))
+        return z.reshape((-1, 1, 28, 28))
 
 
-class BaseVAE(nn.Module):
+class VariationalEncoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalEncoder, self).__init__()
+        self.linear1 = nn.Linear(784, 512)
+        self.linear2 = nn.Linear(512, latent_dims)
+        self.linear3 = nn.Linear(512, latent_dims)
 
-    def __init__(self) -> None:
-        super(BaseVAE, self).__init__()
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc.cuda()  # hack to get sampling on the GPU
+        self.N.scale = self.N.scale.cuda()
+        self.kl = 0
 
-    def encode(self, input: Tensor) -> List[Tensor]:
-        raise NotImplementedError
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu = self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma * self.N.sample(mu.shape)
+        self.kl = (sigma ** 2 + mu ** 2 - torch.log(sigma) - 1 / 2).sum()
+        return z
 
-    def decode(self, input: Tensor) -> Any:
-        raise NotImplementedError
 
-    def sample(self, batch_size: int, current_device: int, **kwargs) -> Tensor:
-        raise NotImplementedError
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = VariationalEncoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
 
-    def generate(self, x: Tensor, **kwargs) -> Tensor:
-        raise NotImplementedError
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
 
-    @abstractmethod
-    def forward(self, *inputs: Tensor) -> Tensor:
-        pass
 
-    @abstractmethod
-    def loss_function(self, *inputs: Any, **kwargs) -> Tensor:
-        pass
+def train(autoencoder, data, epochs=20):
+    opt = torch.optim.Adam(autoencoder.parameters())
+    for epoch in range(epochs):
+        for x, y in data:
+            x = x.to(device) # GPU
+            opt.zero_grad()
+            x_hat = autoencoder(x)
+            loss = ((x - x_hat)**2).sum() + autoencoder.encoder.kl
+            loss.backward()
+            opt.step()
+    return autoencoder
+
+vae = VariationalAutoencoder(latent_dims).to(device) # GPU
+vae = train(vae, data)
---
+++ spectraclass/reduction/vae/network.py
@@ -0,0 +1,248 @@
+import os
+from typing import Any, Dict, List, Tuple, Type, Optional, Union
+from collections import OrderedDict
+
+from functools import partial
+from spectraclass.util.logs import LogManager, lgm, exception_handled, log_timing
+import torch, math
+from torch import Tensor, nn
+import xarray as xa, numpy as np
+from enum import Enum
+
+def tsum(t: Tensor):
+    return torch.sum(t, dim=0, keepdim=False)
+
+def crange( data: xa.DataArray, idim:int ) -> str:
+    sdim = data.dims[idim]
+    c: np.ndarray = data.coords[sdim].values
+    return f"[{c.min():.2f}, {c.max():.2f}]"
+
+def weights_init_uniform_rule(m: nn.Module):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        n = m.in_features
+        y = 1.0 / np.sqrt(n)
+        m.weight.data.uniform_(-y, y)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+def weights_init_xavier(m: nn.Module):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight) # , self.wmag)
+        m.bias.data.fill_(0)
+#           torch.nn.init.uniform_(m.bias, -self.init_bias, self.init_bias)
+class ProcessingStage(Enum):
+    PreTrain = 0
+    Training = 1
+    PostTrain = 2
+    Attribution = 3
+    Evaluation = 4
+
+class NetworkBase(nn.Module):
+
+    def __init__(self, input_dims: int, latent_dims: int, reduction_factor: int, **kwargs):
+        super(NetworkBase, self).__init__()
+        self.device = kwargs.get('device','cpu')
+        self.input_dims = input_dims
+        self.latent_dims = latent_dims
+        self.reduction_factor = reduction_factor
+        self._layer_outputs: Dict[int, List[np.ndarray]] = {}
+        self._layer_weights: Dict[int, List[np.ndarray]] = {}
+        self.hidden_layers = []
+        self.activation = kwargs.get( 'activation', 'lru' )
+        self.build()
+
+    def get_layer_weights(self, iLayer: int) -> np.ndarray:
+        return np.stack(self._layer_weights[iLayer], axis=1)
+
+    def get_layer_output(self, iLayer) -> np.ndarray:
+        return np.stack(self._layer_outputs[iLayer], axis=1)
+
+    @property
+    def top_layer_index(self):
+        return len(self._layer_outputs) - 1
+
+    def _get_activation_function(self, activation: str = None) -> nn.Module:
+        if activation is None: activation = self._activation
+        act = activation.lower()
+        if   act == "relu":         return nn.ReLU()
+        elif act == "lru":          return nn.LeakyReLU( negative_slope=self._actparm )
+        elif act == "celu":         return nn.CELU(self._actparm)
+        elif act == "sigmoid":      return nn.Sigmoid()
+        elif act == "logsigmoid":   return nn.LogSigmoid()
+        elif act == "tanh":         return nn.Tanh()
+        elif act == "softmax":      return nn.Softmax()
+
+    def layer_forward_hook(self, iLayer: int, module: nn.Linear, inputs: Tuple[Tensor], output: Tensor):
+        if (self._stage == ProcessingStage.Training) and ( self._iteration % self._log_step == 0 ):
+            fwts: np.ndarray = module.weight.detach().numpy().copy()
+            self._layer_weights.setdefault(iLayer, []).append(fwts)
+            #            if iLayer == 0: print( f"\nE[{len(self._layer_weights[0])}] wtsamples:  ", end="" )
+            #            print( f"L{iLayer}-{fwts[0][:5]} ", end="")
+            self._layer_outputs.setdefault(iLayer, []).append(output.detach().numpy().copy())
+
+    def layer_forward_pre_hook(self, iLayer: int, module: nn.Linear, inputs: Tuple[Tensor]):
+        self._iteration = self._iteration + 1
+        # lgm().log(f" ** layer_forward_pre_hook[{self._iteration}][{iLayer}]: in_features={module.in_features}, "
+        #              f"out_features={module.out_features}, input_shapes={[tuple(input.size()) for input in inputs]}")
+
+    def add_hidden_layer(self, ilayer: int, layer: nn.Linear, activation: str ):
+        self.hidden_layers.append( layer )
+        torch.nn.init.xavier_uniform_(layer.weight)
+        print(f" * Add linear layer[{ilayer}]: {layer.in_features}->{layer.out_features}")
+        if self.activation != "linear":
+            self.hidden_layers.append( self._get_activation_function(activation) )
+        layer.register_forward_hook(partial(self.layer_forward_hook, ilayer))
+        layer.register_forward_pre_hook(partial(self.layer_forward_pre_hook, ilayer))
+
+    def apply_hidden(self, x: Tensor ) -> Tensor:
+        for layer in self.hidden_layers:
+            x = layer(x)
+        return x
+
+class VariationalEncoder(NetworkBase):
+
+    def __init__(self, input_dims: int, latent_dims: int, reduction_factor: int, **kwargs):
+        super(NetworkBase, self).__init__(input_dims,latent_dims, reduction_factor, **kwargs)
+        self.N = torch.distributions.Normal(0, 1)
+#        self.N.loc = self.N.loc.cuda()  # hack to get sampling on the GPU
+#        self.N.scale = self.N.scale.cuda()
+        self.kl = 0
+        self.build()
+
+    def build(self):
+        in_features, iLayer = self.input_dims, 0
+        while True:
+            out_features = int(round(in_features / self.reduction_factor))
+            if out_features <= self.latent_dims: break
+            linear = nn.Linear(in_features=in_features, out_features=out_features, bias=True)
+            self.add_hidden_layer( iLayer, linear, self.activation )
+            in_features, iLayer = out_features, iLayer + 1
+        self.mu_layer    = nn.Linear(in_features, self.latent_dims)
+        self.sigma_layer = nn.Linear(in_features, self.latent_dims)
+
+    def forward(self, x):
+        x = self.apply_hidden( x )
+        mu = self.mu_layer(x)
+        sigma = torch.exp(self.sigma_layer(x))
+        z = mu + sigma * self.N.sample(mu.shape)
+        self.kl = (sigma ** 2 + mu ** 2 - torch.log(sigma) - 1 / 2).sum()
+        return z
+
+class Decoder(NetworkBase):
+
+    def build(self):
+        in_features, iLayer = self.latent_dims, 0
+        while in_features < self.input_dims:
+            out_features = min( in_features * self.reduction_factor, self.input_dims )
+            linear = nn.Linear( in_features=in_features, out_features=out_features, bias=True )
+            activation = self.activation if (out_features != self.input_dims) else "sigmoid"
+            self.add_hidden_layer(iLayer, linear, activation )
+            in_features, iLayer = out_features, iLayer + 1
+    def forward(self, z):
+       return self.apply_hidden(z)
+
+
+class VariationalAutoencoder():
+
+    def __init__(self, input_dims: int, model_dims: int, **kwargs) -> None:
+        super().__init__()
+        self.input_dims = input_dims
+        self.model_dims = model_dims
+        self.reduction_factor = kwargs.get("reduction_factor",2)
+
+        self._stage = ProcessingStage.PreTrain
+        self._decoder: Decoder = Decoder( input_dims, model_dims, **kwargs ).to(self.device)
+        self._encoder: VariationalEncoder = VariationalEncoder( input_dims, model_dims, **kwargs ).to(self.device)
+
+    def encoder(self, input: Tensor ) -> Tensor:
+        return self._encoder( input )
+
+    def decoder(self, input: Tensor) ->  Tensor:
+        return self._decoder( input )
+
+
+    def training_step_end(self, step_output):
+        return step_output
+
+    def predict(self, data: xa.DataArray) -> xa.DataArray:
+        input: Tensor = torch.from_numpy(data.values)
+        result: np.ndarray = self.forward(input).detach()
+        return xa.DataArray(result, dims=['samples', 'y'], coords=dict(samples=data.coords['samples'], y=range(result.shape[1])), attrs=data.attrs)
+
+    @exception_handled
+    def encode(self, data: np.ndarray, **kwargs) -> Union[np.ndarray,Tensor]:
+        detach = kwargs.get('detach',False)
+        input: Tensor = torch.from_numpy(data)
+        result: Tensor = self.encoder(input)
+        return result.detach().numpy() if detach else result
+
+    @exception_handled
+    def decode(self, data: Union[np.ndarray,Tensor]) -> np.ndarray:
+        input: Tensor = torch.from_numpy(data) if (type(data) == np.ndarray) else data
+        result: Tensor = self.decoder(input)
+        return result.detach().numpy()
+
+    def train( self, data, epochs=20):
+        opt = torch.optim.Adam( self.parameters() )
+        for epoch in range(epochs):
+            for x, y in data:
+                opt.zero_grad()
+                x_hat = self.forward(x)
+                loss = ((x - x_hat) ** 2).sum() + self.encoder.kl
+                loss.backward()
+                opt.step()
+
+    def forward(self, x: Tensor) -> Tensor:
+        encoded: Tensor = self.encoder(x)
+        result = self.decoder(encoded)
+        return result
+
+    @property
+    def network_type(self) -> str:
+        return f"{self.input_dims}-{self.model_dims}"
+
+    @property
+    def results_dir(self):
+        from spectraclass.data.base import DataManager, dm
+        return dm().cache_dir
+
+    def save(self, name: str):
+        models_dir = f"{self.results_dir}/models"
+        os.makedirs(models_dir, exist_ok=True)
+        try:
+            model_path = f"{models_dir}/{name}.encoder.{self.network_type}.pth"
+            torch.save(self._encoder.state_dict(), model_path )
+            print(f"Saved encoder to file '{model_path}'" )
+            model_path = f"{models_dir}/{name}.decoder.{self.network_type}.pth"
+            torch.save(self._decoder.state_dict(), model_path)
+            print(f"Saved decoder to file '{model_path}'")
+        except Exception as err:
+            print(f"Error saving model {name}: {err}")
+
+    def network( self, type: str ) -> NetworkBase:
+        if type == "encoder":   return self._encoder
+        elif type == "decoder": return self._decoder
+        else: raise Exception( f"Unlnown nnet type: {type}")
+
+    def load_weights(self, type: str, filepath: str ):
+        weights = torch.load(filepath)
+        self.network(type).load_state_dict(weights)
+        print(f"Loaded {type} weights from file '{filepath}'")
+
+    def load(self, name: str, **kwargs) -> bool:
+        models_dir = f"{self.results_dir}/models"
+        os.makedirs(models_dir, exist_ok=True)
+        mpaths = []
+        try:
+            for mtype in [ "encoder", "decoder" ]:
+                model_path = f"{models_dir}/{name}.{mtype}.{self.network_type}.pth"
+                mpaths.append( model_path )
+                self.load_weights( mtype, model_path )
+        except Exception as err:
+            lgm().log(f"Error loading model {name} ({mpaths[-1]}):\n  ---> {err}")
+            return False
+        lgm().log(f"Loaded MODEL {name}: {mpaths}")
+        return True
+
+
