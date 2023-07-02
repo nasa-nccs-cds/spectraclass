dd9d0930057dd863e5aa1623d3a31901e0166773
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-07-01 19:04:24 -0600

    Add select buttons for BlockSelectMode

--- spectraclass/gui/spatial/neon/manager.py
+++ spectraclass/gui/spatial/neon/manager.py
@@ -100,7 +100,7 @@ class NEONTileSelector:
                 ufm().clear()
                 new_selected_rects = [self.rect0]
             else:
-                self.selected_rectangles.append( self.rect0 )
+                self.selected_rectangles.append(self.rect0)
                 new_selected_rects = self.selected_rectangles
         return hv.Rectangles( new_selected_rects ).opts( line_color="white", fill_alpha=0.2, line_alpha=1.0, line_width=3 )
 
@@ -119,17 +119,19 @@ class NEONTileSelector:
         for block in blocks:
             (bxlim, bylim) = block.get_extent( spm().projection )
             r = (bxlim[0],bylim[0],bxlim[1],bylim[1])
-            self.rect_grid[ block.block_coords ] =  r
+            self.rect_grid[ block.block_coords] =  r
         lgm().log( f"TS: nblocks={len(blocks)}, nindices={len(self.rect_grid)}, indices={list(self.rect_grid.keys())}")
-        self.rect0 = self.rect_grid[ tm().block_index ]
+        self.rect0 = tm().block_index
         basemap = spm().get_image_basemap( self.xlim + self.ylim )
-        self.rectangles = hv.Rectangles( list(self.rect_grid.values()) ).opts( line_color="cyan", fill_alpha=0.0, line_alpha=1.0 )
-        image = basemap * self.rectangles * self.selected_rec * self.selection_boxes
+        self.rectangles = hv.Rectangles(list(self.rect_grid.values())).opts(line_color="cyan", fill_alpha=0.0, line_alpha=1.0)
+        image = basemap * self.rectangles * self.selected_rec
         if self.selection_mode == BlockSelectMode.LoadTile:
             return image
         else:
+            region_selection = self.selection_boxes.opts(
+                opts.Rectangles(active_tools=['box_edit'], fill_alpha=0.5, line_alpha=1.0, line_color="white", fill_color="white"))
             selection_panel = self.get_selection_panel()
-            return pn.Column( image, selection_panel )
+            return pn.Column( image * region_selection, selection_panel )
 
     @exception_handled
     def block_index(self, x, y ) -> Tuple[int,int]:
