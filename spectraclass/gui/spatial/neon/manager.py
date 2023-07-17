5a634c165392859dd30005dfdd26b5ea21900f96
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-07-17 17:00:32 -0600

    Update selection gui

--- spectraclass/gui/spatial/neon/manager.py
+++ spectraclass/gui/spatial/neon/manager.py
@@ -304,21 +304,22 @@ class NEONTileSelector(param.Parameterized):
         return clm().panel()
 
     def gui( self, **kwargs ):
+        selection_mode = kwargs.get( "mode", self.selection_mode )
         self.rect0 = tm().block_index
         basemap = spm().get_image_basemap( self.blockSelection.region_bounds )
         self.rect_grid = self.blockSelection.grid_widget()
         image = basemap * self.rect_grid * self.selected_rec
-        if self.selection_mode == BlockSelectMode.LoadTile:
+        if selection_mode == BlockSelectMode.LoadTile:
             return image
         else:
             selection_panel = self.get_control_panel()
-            if self.selection_mode == BlockSelectMode.SelectTile:
+            if selection_mode == BlockSelectMode.SelectTile:
                 return pn.Row( image * self.region_selection, selection_panel )
-            elif self.selection_mode == BlockSelectMode.CreateMask:
+            elif selection_mode == BlockSelectMode.CreateMask:
                 cluster_panel = self.get_cluster_panel()
                 viz_panels = pn.Tabs( ("select", image * self.region_selection), ("cluster", cluster_panel))
                 return pn.Row( viz_panels, selection_panel )
-            elif self.selection_mode == BlockSelectMode.LoadMask:
+            elif selection_mode == BlockSelectMode.LoadMask:
                 return self.get_block_selection_gui()
 
     @property
