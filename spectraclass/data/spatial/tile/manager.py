49107f0ac26695cc64a2a076a00b27c9201a5c46
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-07-31 12:50:50 -0600

    Add "apply mask"

--- spectraclass/data/spatial/tile/manager.py
+++ spectraclass/data/spatial/tile/manager.py
@@ -123,11 +123,10 @@ class TileManager(SCSingletonConfigurable):
     def get_folium_map(self, block: Block  ) -> folium.Map:
         tile_url='http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
         fmap = folium.Map( width=self.map_size )
-        data_crs = block.data.spatial_ref.attrs['crs_wkt']
-        geotrans = Transformer.from_crs( data_crs, f'epsg:4326')
+        geotrans = Transformer.from_crs( block.wkt, f'epsg:4326')
         x0, x1, y0, y1 =  block.extent
         bounds = [ geotrans.transform(  x0, y0 ), geotrans.transform(  x1, y1 ) ]
-        lgm().log( f"#FM: get_folium_map: block.extent={x0, x1, y0, y1}, bounds={bounds}, data_crs={data_crs}")
+        lgm().log( f"#FM: get_folium_map: block.extent={x0, x1, y0, y1}, bounds={bounds}, data_crs={block.wkt}")
         fmap.fit_bounds(bounds)
         map_attrs = dict( url=tile_url, layers='World Imagery', transparent=False, control=False, fmt="image/png",
                           name='Satellite Image', overlay=True, show=True )
