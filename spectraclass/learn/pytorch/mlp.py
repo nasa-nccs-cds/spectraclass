eb230b60e841a9ce10d44f14b3d40170c8628f0f
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-07-30 20:48:29 -0600

    Add "apply mask"

--- spectraclass/learn/pytorch/mlp.py
+++ spectraclass/learn/pytorch/mlp.py
@@ -82,7 +82,7 @@ class MLP(nn.Module):
         lgm().log(f"#MPL: DONE BUILDING NETWORK: {self.input_dims} -> {self.nclasses}")
 
     def get_dtype(self) -> np.dtype:
-        wts: np.ndarray = self._network[0].weight.detach().numpy()
+        wts: np.ndarray = self.network.weight.detach().numpy()
         return wts.dtype
 
     def init_weights(self):
