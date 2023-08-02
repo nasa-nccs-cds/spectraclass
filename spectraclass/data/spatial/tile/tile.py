defd15b3b524982f751200ba11315566c6443013
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-08-02 15:54:07 -0600

    Add "apply mask"

--- spectraclass/data/spatial/tile/tile.py
+++ spectraclass/data/spatial/tile/tile.py
@@ -93,7 +93,7 @@ class DataContainer:
 #        gt = self.data.attrs['transform']
         (xr, yr) = ( self.xrange, self.yrange ) if self._data_projected else self.transformer.transform( self.xrange, self.yrange  )
         dx, dy = (xr[1]-xr[0])/(self.data.shape[-1]-1), (yr[1]-yr[0])/(self.data.shape[-2]-1)
-        self._extent = ( xr[0]-dx/2,  xr[-1]+dx/2,  yr[-1]-dy/2,  yr[0]+dy/2 )
+        self._extent = ( xr[0]-dx/2,  xr[-1]+dx/2,  yr[0]+dy/2, yr[-1]-dy/2 )
         self._transform = ( dx, 0, xr[0], 0, dy, yr[0] )
 
     @property
