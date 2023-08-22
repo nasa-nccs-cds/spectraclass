6ac58261de6dc42ff7be5e20de087e9e2508ffb6
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-08-22 13:34:09 -0600

    Add load labelset

--- spectraclass/learn/cluster/manager.py
+++ spectraclass/learn/cluster/manager.py
@@ -432,7 +432,7 @@ class ClusterManager(SCSingletonConfigurable):
             if source == "model":
                 input_data: xa.DataArray = block.getModelData(raster=False)
             else:
-                input_data: xa.DataArray = block.get_point_data(class_filter=False, norm="spectral")
+                input_data: xa.DataArray = block.get_point_data(class_filter=False)
                 lgm().log( f"#CM.generate_training_set[{block_coords}]: input_data{input_data.shape}[{input_data.dtype}] "
                            f"stat={stat(input_data)}, icluster={icluster}, nclusters={nclusters}, ngids={marker.gids.size} ")
             mask_array: np.array = np.full( input_data.shape[0], False, dtype=bool )
