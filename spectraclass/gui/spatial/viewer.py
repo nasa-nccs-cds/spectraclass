2cf1d29606f162d4a554313703b0e60424189dff
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-08-22 12:31:51 -0600

    Add load labelset

--- spectraclass/gui/spatial/viewer.py
+++ spectraclass/gui/spatial/viewer.py
@@ -374,20 +374,14 @@ class hvSpectraclassGui(SCSingletonConfigurable):
     def get_data( self, cname: str, **kwargs ) -> xa.DataArray:
         from spectraclass.data.spatial.tile.manager import TileManager, tm
         from spectraclass.data.base import dm
-        sfactor = kwargs.get('sfactor', 2.0)
         block: Block = tm().getBlock( **kwargs )
-        lgm().log( f"sgui:get_data[{cname}] block = {block.index}")
-        if cname=="bands":
-            result =  tm().prepare_inputs(block=block, raster=True, **kwargs)
-            result.attrs['clim'] = (-self.color_range,self.color_range)
-        elif cname=="features":
-            result = dm().getModelData(block=block, raster=True, norm=True)
-            result.attrs['clim'] = self.get_clim( result.values, sfactor )
-        elif cname=="reproduction":
-            result = block.getReproduction(raster=True)
-            result.attrs['clim'] = (-self.color_range,self.color_range)
+
+        if   cname == "bands":        result = tm().prepare_inputs(block=block, raster=True, **kwargs)
+        elif cname == "features":     result = dm().getModelData(block=block, raster=True, norm=True)
+        elif cname == "reproduction": result = block.getReproduction(raster=True)
         else:
             raise Exception( f"Unkonwn data type: {cname}")
+        result.attrs['clim'] = (-self.color_range, self.color_range)  # sfactor = kwargs.get('sfactor', 2.0); self.get_clim( result.values, sfactor )
         return result
 
     def get_clim(self, data: np.ndarray, sfactor: float) -> Tuple[float,float]:
