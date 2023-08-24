a92e5bf5e01aabc4ad7e977a793905537e663f92
Thomas Maxwell <thomas.maxwell@nasa.gov>
2023-08-24 14:49:16 -0600

    Add load labelset

--- spectraclass/reduction/ca.py
+++ spectraclass/reduction/ca.py
@@ -32,6 +32,11 @@ class PCAReducer:
         self.mean = self._model.mean_
         lgm().log( f"#PCA.train: components shape={self.components.shape}, mean shape= {self.mean.shape}, "
                    f"train_input stat={stat(train_input)}, mean stat={stat(self.mean)}, components stat={stat(self.components)}" )
+        reduced:      np.ndarray = self.get_reduced_features(train_input)
+        reproduction: np.ndarray = self.get_reproduction(reduced)
+        lgm().log(f"#PCA.train: reduced shape={reduced.shape}, stat={stat(reduced)}")
+        lgm().log(f"#PCA.train: reproduction shape={reproduction.shape}, stat={stat(reproduction)}")
+        lgm().log(f"#PCA.train: diff stat={stat(np.abs(reproduction-train_input))}")
 
     def get_reduced_features(self, train_input: np.ndarray ) -> np.ndarray:
         centered_train_input: np.ndarray = train_input - self.mean
